76b7fa3166df14f61377750b922a627d01c741c3
Lynx2711 <aditiverma11448@gmail.com>
2024-08-27 18:29:01 +0530

    done

---
+++ assignments/question3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int square = num * num;
+
+    cout << "The square of " << num << " is: " << square << endl;
+}
